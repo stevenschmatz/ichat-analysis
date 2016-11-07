538d778e1e903dc9d0d451028967eb22a1ca4e3c
Steven Schmatz <stevenschmatz@gmail.com>
2016-11-06 21:50:04 -0500

    function to get all messages from a given conversation

--- src/app.js
+++ src/app.js
@@ -1,3 +1,20 @@
+import path from 'path';
+import nconf from 'nconf';
 import ChatDBManager from './model/ChatDBManager';
 
-let chatManager = new ChatDBManager();
+function init() {
+  nconf.argv().env().file({file: path.join(__dirname, "config.json")});
+}
+
+function main() {
+  let chatManager = new ChatDBManager();
+  const identifier = nconf.get("debugEmail");
+
+  chatManager.getAllMessagesForIdentifier(identifier).then(messageData => {
+    console.log(messageData.length);
+  });
+}
+
+init();
+main();
+
--- src/model/ChatDBManager.js
+++ src/model/ChatDBManager.js
@@ -1,7 +1,88 @@
 // ChatDBManager.js
 
+import process from 'process';
+import path from 'path';
+import nconf from 'nconf';
+
+const sqlite3 = require('sqlite3').verbose();
+
 export default class ChatDBManager {
   constructor() {
-    const dbPath = "~/Library/Messages/chat.db";
+    let dbPath;
+
+    if (nconf.get("debug")) {
+      dbPath = nconf.get("dbPath");
+    } else {
+      dbPath = path.join(process.env['HOME'], "Library/Messages/chat.db");
+    }
+
+    this.db = new sqlite3.Database(dbPath, sqlite3.OPEN_READONLY);
+  }
+
+  query(query) {
+    return new Promise((resolve, reject) => {
+      this.db.all(query, (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  }
+
+  debug() {
+    const query = "SELECT text FROM message LIMIT 10";
+
+    this.query(query).then(result => {
+      const messages = result.map(row => row.text);
+      console.log(messages);
+    }).catch(err => {
+      console.log(err);
+    });
+  }
+
+  allChatIdentifiers() {
+    const query = "SELECT chat_identifier FROM chat";
+    
+    return new Promise((resolve, reject) => {
+      this.query(query).then(result => {
+        resolve(result.map(record => record.chat_identifier));
+      }).catch(err => {
+        reject(err);
+      });
+    });
+  }
+
+  /**
+   * Returns all message data for the given identifier.
+   * @param {string} identifier the ID of the conversation recipient
+   * @return {array} an array of (is_from_me, text) pairs for the given person.
+   */
+  getAllMessagesForIdentifier(identifier) {
+    const query = `
+      SELECT * FROM message where handle_id = (
+        SELECT handle_id FROM chat_handle_join WHERE chat_id = (
+          SELECT ROWID FROM chat WHERE guid='iMessage;-;${identifier}'
+        )
+      )
+    `;
+
+    return new Promise((resolve, reject) => {
+      this.query(query).then(result => {
+        const filteredResult = result.map(entry => {
+          return {
+            text: entry.text,
+            is_from_me: entry.is_from_me === 1 ? true : false,
+            date: new Date(entry.date),
+            attachment: entry.cache_has_attachments === 1 ? true : false
+          };
+        });
+
+        resolve(filteredResult);
+      }).catch(err => {
+        reject(err);
+      });
+    });
   }
 }
